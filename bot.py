9b757298f43cd8a6575e7a612c04ebafef937be7
AnionCation <captainanioncation@gmail.com>
2016-01-10 14:40:49 +0000

    getChannel() now returns channel to send to
    getChannel() previously simply returned the channel which a message was
    sent from, however this meant if a private message was sent it would
    return the result of the command to itself - channel <botnick>, which is
    not useful. Therefore it checks if the channel is <botnick> and if so,
    it will instead return the username of the user who sent the private
    message to the bot.

--- bot.py
+++ bot.py
@@ -38,9 +38,15 @@ def getChannel(text):
             text: String of data, raw command that comes from irc.
 
         Returns: "#<CHANNEL NAME>"
+        If the channel the message is sent to is the name of the bot (AKA it is a PM)
+        Then Returns: "<Username>" where <Username> is the username of the user
+        who PMd the bot
     '''
     parts = text.split(":")
-    return ((parts[len(parts)-2]).split(" ")[2])
+    if (parts[len(parts)-2]).split(" ")[2] == botnick.upper():
+        return((parts[len(parts)-2]).split(" ")[0].split("!")[0])
+    else:
+        return ((parts[len(parts)-2]).split(" ")[2])
 
 def makeDeck(deck):
     '''
