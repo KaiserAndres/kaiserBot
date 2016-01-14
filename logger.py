bf1d94d2f720430c20d9d88d4e4baa6c39484194
Andres Villagra de la Fuente <andres_vf@icloud.com>
2016-01-14 19:43:33 -0300

    Made reader class.
    This class listens for the specific channel it's linked to

--- logger.py
+++ logger.py
@@ -22,14 +22,55 @@ def getFreeId():
 		newId = open("logIds.dat", "r")
 		idNumber = int(newId.read())
 		newId.close()
-		newId = open("logIds.dat", "w")
-		newId.write(str(idNumber+1))
-		newId.close()
+		updateFreeId(idNumber)
 	except:
+		#-----------------------------------------------------------------------
+		#	Makes a new file in case one isn't found.
+		#-----------------------------------------------------------------------
 		idNumber = 0
-		newId = open("logIds.dat", "w")
-		newId.write(str(idNumber+1))
-		newId.close()
+		updateFreeId(idNumber)
 
 	return idNumber
+
+
+def updateFreeId(lastNumber, change = 1):
+	'''
+		Updates the value in the logIds file.
+
+		lastNumber: Integer of the last used id
+		change: Integer, describes how many numbers are skipped.
+	'''
+	newId = open("logIds.dat", "w")
+	newId.write(str(lastNumber+change))
+	newId.close()
+
+class reader:
+
+	'''
+		logs the conversation and saves it to a file
+	'''
+	def __init__(self, room, logId = getFreeId()):
+		'''	
+			room: String with the following format:
+				#<ROOM NAME>
+				  This is the irc room that is listened for recording messages.
+			logId: Integer with the last free id, unless specified will always
+				   be the last available id.
+		'''
+		self.room = room
+		self.logFile = open(str(logId)+".log", "w")
+
+	def recordLine(self, user, message):
+		'''
+			Records the message from the user in the specific file
+
+			user: string, name of the user.
+			message: string, data sent by the user.
+		'''
+		try:
+			if message[:2] != "((":
+				self.logFile.write(user+": "+message+"\n")
+		except:
+			pass
+
 
