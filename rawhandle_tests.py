6560eab85e8cfb2298a7a78073d99e63d2926ab4
Andres Villagra de la Fuente <andres_vf@icloud.com>
2017-12-31 19:46:45 -0300

    Reversed the test calls, they were backwards, oops.

--- rawhandle_tests.py
+++ rawhandle_tests.py
@@ -8,40 +8,40 @@ BOT_NAME = "kaiTest"
 class MessageTest(unittest.TestCase):
     def test_ping_identity(self):
         message = Message("PING :300472EC", BOT_NAME)
-        self.assertEqual(message.text, "PING :300472EC")
+        self.assertEqual("PING :300472EC", message.text)
 
     def test_ping_username_is_empty(self):
         message = Message("PING :300472EC", BOT_NAME)
-        self.assertEqual(message.userName, "")
+        self.assertEqual("", message.userName)
 
     def test_timeout_message_empty_username(self):
         message = Message(":CLOSING LINK: HOST55.170-80-170.NETWORK.NET (CONNECTION TIMED OUT)", BOT_NAME)
-        self.assertEqual(message.userName, "")
+        self.assertEqual("", message.userName)
 
     def test_username(self):
         message = Message(":KAISER_!~QUASSEL@HOST55.170-80-170.NETWORK.NET PRIVMSG #KAISER_TEST :!TAROT 6", BOT_NAME)
-        self.assertEqual(message.userName, "KAISER_")
+        self.assertEqual("KAISER_", message.userName)
 
     def test_text(self):
         message = Message(":KAISER_!~QUASSEL@HOST55.170-80-170.NETWORK.NET PRIVMSG #KAISER_TEST :!TAROT 1", BOT_NAME)
-        self.assertEqual(message.text, "!TAROT 1")
+        self.assertEqual("!TAROT 1", message.text,)
 
     def test_public_channel(self):
         message = Message(":KAISER_!~QUASSEL@HOST55.170-80-170.NETWORK.NET PRIVMSG #KAISER_TEST :!TAROT 1", BOT_NAME)
-        self.assertEqual(message.channel, "#KAISER_TEST")
+        self.assertEqual("#KAISER_TEST", message.channel)
 
     def test_private_channel(self):
         message = Message(":KAISER_!~QUASSEL@HOST92.170-80-171.NETWORK.NET PRIVMSG " + BOT_NAME + " :!ROLL 1D20", BOT_NAME)
-        self.assertEqual(message.channel, message.userName)
+        self.assertEqual(message.userName, message.channel)
 
     def test_frowny_face_bug(self):
         message = Message(":OREA!KIWIIRC@CPC1166-HEHE14-2-0-CUST57.9-1.CABLE.VIRGINM.NET PRIVMSG #RPGSTUCK_REDLOTUS::C", BOT_NAME)
-        self.assertEqual(message.channel, "#RPGSTUCK_REDLOTUS")
+        self.assertEqual("#RPGSTUCK_REDLOTUS", message.channel)
 
     def test_message_with_url(self):
         text = ":SEAN_AYRES!WEBCHAT@D162-156.NETWORK:COM PRIVMSG #RPGSTUCK_REDLOTUS :HTTPS://PASTEBIN.COM/A6ECRGD7"
         message = Message(text, BOT_NAME)
-        self.assertEqual(message.text, "HTTPS://PASTEBIN.COM/A6ECRGD7")
+        self.assertEqual("HTTPS://PASTEBIN.COM/A6ECRGD7", message.text)
 
 
 if __name__ == '__main__':
